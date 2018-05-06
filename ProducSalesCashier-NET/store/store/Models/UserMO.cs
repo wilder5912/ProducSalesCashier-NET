fe5b0ef4f8c4cbbe63b050d56b796c707a7a1e18
wilder5912 <wilder.5912@gmail.com>
2018-05-05 23:36:14 -0400

    create form of user

---
+++ ProducSalesCashier-NET/store/store/Models/UserMO.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace store.Models
+{
+    public class UserMO
+    {
+        public decimal userId { get; set; }
+        [Required]
+        private string firtNameUser;
+        [Required]
+        private string lastNameUser;
+        [Required]
+        private string emailUser;
+        [Required]
+        private string passwordUser;
+        [Required]
+        private string typeUser;
+        private string imagenUser;
+        private string tokenUser;
+        private string codeUser;
+
+        [Required]
+        public string FirtNameUser
+        {
+            get
+            {
+                return firtNameUser;
+            }
+
+            set
+            {
+                firtNameUser = value;
+            }
+        }
+
+        public string LastNameUser
+        {
+            get
+            {
+                return lastNameUser;
+            }
+
+            set
+            {
+                lastNameUser = value;
+            }
+        }
+
+        public string EmailUser
+        {
+            get
+            {
+                return emailUser;
+            }
+
+            set
+            {
+                emailUser = value;
+            }
+        }
+
+        public string PasswordUser
+        {
+            get
+            {
+                return passwordUser;
+            }
+
+            set
+            {
+                passwordUser = value;
+            }
+        }
+
+        public string TypeUser
+        {
+            get
+            {
+                return typeUser;
+            }
+
+            set
+            {
+                typeUser = value;
+            }
+        }
+
+        public string ImagenUser
+        {
+            get
+            {
+                return imagenUser;
+            }
+
+            set
+            {
+                imagenUser = value;
+            }
+        }
+
+        public string TokenUser
+        {
+            get
+            {
+                return tokenUser;
+            }
+
+            set
+            {
+                tokenUser = value;
+            }
+        }
+
+        public string CodeUser
+        {
+            get
+            {
+                return codeUser;
+            }
+
+            set
+            {
+                codeUser = value;
+            }
+        }
+
+       
+
+     
+    }
+    //[MetadataType(typeof(UserMO))]
+    //public partial class user
+    //{
+
+    //}
+}
---
+++ ProducSalesCashier-NET/store/store/service/UserService.cs
@@ -0,0 +1,25 @@
+﻿using store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace store.service
+{
+    public class UserService
+    {
+        public user getUser(UserMO userMO)
+        {
+            user userData = new user();
+            userData.firt_name_user = userMO.FirtNameUser;
+            userData.last_name_user = userMO.LastNameUser;
+            userData.email_user = userMO.EmailUser;
+            userData.password_user = userMO.PasswordUser;
+            userData.type_user = userMO.TypeUser;
+            userData.imagen_user = userMO.ImagenUser;
+            userData.token_user = userMO.TokenUser;
+
+            return userData;
+        }
+    }
+}
