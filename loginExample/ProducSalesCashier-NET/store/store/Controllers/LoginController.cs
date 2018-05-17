651e862086bfe741648c9c7cc1e9d3ec2aa7ef47
wilder5912 <wilder.5912@gmail.com>
2018-05-16 22:19:48 -0400

    example of login

---
+++ loginExample/ProducSalesCashier-NET/store/store/Controllers/LoginController.cs
@@ -0,0 +1,72 @@
+﻿using Helper;
+using store.Models;
+using store.service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace store.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private UserService userService;
+
+        public LoginController()
+        {
+            userService = new UserService();
+        }
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(UserMO userMO)
+        {
+            UserMO userCurrent = new UserMO();
+            userCurrent = userService.getUserInformation(userMO);
+
+            if (null != userCurrent) {
+
+                int Usuario_id = (int)userCurrent.userId;
+                SessionHelper.AddUserToSession(userCurrent.userId + "");
+               // SessionHelper.AddUserToSession(userCurrent.EmailUser+ "");
+
+                return Redirect("~/Home");
+            }
+           ModelState.AddModelError("", "error en el usuario");
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult Autentificar(UserMO userMO)
+        {
+            // Guardamos un usuario en SESSION, en teoría tiene el ID número 2001
+            int Usuario_id = 2001;
+
+            SessionHelper.AddUserToSession(Usuario_id.ToString());
+
+            return Redirect("~/Home");
+        }
+
+
+        public ActionResult Autentificado()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            SessionHelper.DestroyUserSession();
+            return Redirect("~/Login");
+        }
+
+    }
+}
---
+++ loginExample/ProducSalesCashier-NET/store/store/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using store.Models;
+using store.service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace store.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserService userService;
+
+        public UserController()
+        {
+            userService = new UserService();
+        }
+        // GET: User
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult addUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult addUser(UserMO userMO)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                using (var db = new storeEntities())
+                {
+
+                    db.user.Add(userService.getUser(userMO));
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("", "error al agregar el alumno" + ex.Message);
+                return View();
+
+            }
+        }
+
+    }
+}
